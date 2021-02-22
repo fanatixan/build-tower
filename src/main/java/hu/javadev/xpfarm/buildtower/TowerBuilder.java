e62cf1dd734f08b259b6ddfe7719184c77d02b0c
fejera <attila@javadev.hu>
2021-02-22 22:47:31 +0100

    refactor: renamed methods for better readability

--- src/main/java/hu/javadev/xpfarm/buildtower/TowerBuilder.java
+++ src/main/java/hu/javadev/xpfarm/buildtower/TowerBuilder.java
@@ -8,22 +8,22 @@ public class TowerBuilder {
     public List<String> build(int numberOfLevels) {
         List<String> result = new ArrayList<>();
         for (int i = 1; i <= numberOfLevels; i++) {
-            final int marginWidth = toMarginWidth(numberOfLevels, i);
-            final String level = " ".repeat(marginWidth) + "*".repeat(toLevelWidth(i)) + " ".repeat(marginWidth);
+            final int marginWidth = marginWidth(numberOfLevels, i);
+            final String level = " ".repeat(marginWidth) + "*".repeat(levelWidth(i)) + " ".repeat(marginWidth);
             result.add(level);
         }
         return result;
     }
 
-    private int toMarginWidth(int numberOfLevels, int level) {
-        return (toWidth(numberOfLevels) - toLevelWidth(level)) / 2;
+    private int marginWidth(int numberOfLevels, int level) {
+        return (width(numberOfLevels) - levelWidth(level)) / 2;
     }
 
-    private int toLevelWidth(int level) {
+    private int levelWidth(int level) {
         return level * (level + 1) / 2;
     }
 
-    private int toWidth(int numberOfLevels) {
+    private int width(int numberOfLevels) {
         return numberOfLevels * 2 - 1;
     }
 
