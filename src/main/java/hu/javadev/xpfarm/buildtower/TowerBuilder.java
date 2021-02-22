6d6a166f6a9ad0ebc4ecc11d20c35f7a5004e1fd
fejera <attila@javadev.hu>
2021-02-22 22:44:05 +0100

    refactor: extracted witdh calculation to method

--- src/main/java/hu/javadev/xpfarm/buildtower/TowerBuilder.java
+++ src/main/java/hu/javadev/xpfarm/buildtower/TowerBuilder.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class TowerBuilder {
 
     public List<String> build(int numberOfLevels) {
-        final int width = numberOfLevels * 2 - 1;
+        final int width = toWidth(numberOfLevels);
         List<String> result = new ArrayList<>();
         for (int i = 1; i <= numberOfLevels; i++) {
             final int levelWidth = i * (i + 1) / 2;
@@ -15,6 +15,10 @@ public class TowerBuilder {
             result.add(level);
         }
         return result;
+    }
+
+    private int toWidth(int numberOfLevels) {
+        return numberOfLevels * 2 - 1;
     }
 
 }
