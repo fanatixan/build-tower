67fe122e112cfda5a0c2d80d10e7185cb13a91da
fejera <attila@javadev.hu>
2021-02-22 22:30:00 +0100

    made test fail: invoking non-existent method

--- src/test/java/hu/javadev/xpfarm/buildtower/TowerBuilderTest.java
+++ src/test/java/hu/javadev/xpfarm/buildtower/TowerBuilderTest.java
@@ -1,7 +1,24 @@
 package hu.javadev.xpfarm.buildtower;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class TowerBuilderTest {
 
     private TowerBuilder towerBuilder;
+
+    @BeforeEach
+    void setup() {
+        towerBuilder = new TowerBuilder();
+    }
+
+    @Test
+    void givenOneLevel_build_shouldReturnOneSizeList() {
+        // given
+        int numberOfLevels = 1;
+
+        // when
+        towerBuilder.build(numberOfLevels);
+    }
 
 }
