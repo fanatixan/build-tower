c07eed765a0e4aa41be9496f5b3d32c8e653db2d
fejera <attila@javadev.hu>
2021-02-22 22:38:40 +0100

    green: naive implementation - correct number of levels, all look the same

--- NOTES.md
+++ NOTES.md
@@ -5,7 +5,7 @@ Due to specification: number of floors is integer and always greater than 0.
 Test Cases:
 - ✅ 1 floor --> 1 element list
 - ✅ 1 floor --> first element equals to `*`
-- 2 floors --> 2 element list
+- ✅ 2 floors --> 2 element list
 - 2 floors --> result equals to 
   ```text
    * 
--- src/main/java/hu/javadev/xpfarm/buildtower/TowerBuilder.java
+++ src/main/java/hu/javadev/xpfarm/buildtower/TowerBuilder.java
@@ -1,11 +1,16 @@
 package hu.javadev.xpfarm.buildtower;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TowerBuilder {
 
     public List<String> build(int numberOfLevels) {
-        return List.of("*");
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < numberOfLevels; i++) {
+            result.add("*");
+        }
+        return result;
     }
 
 }
