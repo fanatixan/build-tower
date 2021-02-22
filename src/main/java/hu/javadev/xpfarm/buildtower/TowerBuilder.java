1c1a95f8e55ebce269548ac63cb58401be6a119a
fejera <attila@javadev.hu>
2021-02-22 22:42:56 +0100

    green: made test pass

--- src/main/java/hu/javadev/xpfarm/buildtower/TowerBuilder.java
+++ src/main/java/hu/javadev/xpfarm/buildtower/TowerBuilder.java
@@ -6,9 +6,13 @@ import java.util.List;
 public class TowerBuilder {
 
     public List<String> build(int numberOfLevels) {
+        final int width = numberOfLevels * 2 - 1;
         List<String> result = new ArrayList<>();
-        for (int i = 0; i < numberOfLevels; i++) {
-            result.add("*");
+        for (int i = 1; i <= numberOfLevels; i++) {
+            final int levelWidth = i * (i + 1) / 2;
+            final int marginWidth = (width - levelWidth) / 2;
+            final String level = " ".repeat(marginWidth) + "*".repeat(levelWidth) + " ".repeat(marginWidth);
+            result.add(level);
         }
         return result;
     }
