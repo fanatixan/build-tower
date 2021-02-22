a5444cb09f487be13cb91cd5b4d07b4346e2b987
fejera <attila@javadev.hu>
2021-02-22 22:44:27 +0100

    refactor: inlined width variable

--- src/main/java/hu/javadev/xpfarm/buildtower/TowerBuilder.java
+++ src/main/java/hu/javadev/xpfarm/buildtower/TowerBuilder.java
@@ -6,11 +6,10 @@ import java.util.List;
 public class TowerBuilder {
 
     public List<String> build(int numberOfLevels) {
-        final int width = toWidth(numberOfLevels);
         List<String> result = new ArrayList<>();
         for (int i = 1; i <= numberOfLevels; i++) {
             final int levelWidth = i * (i + 1) / 2;
-            final int marginWidth = (width - levelWidth) / 2;
+            final int marginWidth = (toWidth(numberOfLevels) - levelWidth) / 2;
             final String level = " ".repeat(marginWidth) + "*".repeat(levelWidth) + " ".repeat(marginWidth);
             result.add(level);
         }
